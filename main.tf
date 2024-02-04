346d2e96fd10e7614adb5c582154afd291b0d25c
Dujon91 <153690132+Dujon91@users.noreply.github.com>
2024-02-04 16:21:58 +0000

    test

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ terraform {
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-dujon" 
+}
 resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"  # Change to the desired instance type
