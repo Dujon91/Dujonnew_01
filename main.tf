b09268ca1d7bc848f8c4c1045735d1a1a48d50e0
Dujon91 <153690132+Dujon91@users.noreply.github.com>
2024-02-04 12:42:12 +0000

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-test
+provider "aws" {
+  region = "eu-west-2" # London region
+}
+resource "aws_instance" "test5" {
+  ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+  instance_type = "t2.micro"  # Change to the desired instance type
+  tags = {
+    Name = "sundayexample-instance"
+  }
+}
